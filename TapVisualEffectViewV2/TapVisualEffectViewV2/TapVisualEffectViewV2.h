8360bc89ea84decb922cf27751b1ba87cbe3a609
O Rabie <o.rabie@tap.company>
2020-09-22 11:01:54 +0200

    Upgraded TapVisualEffectViewKit to Xcode 12+ comiler friendly code

---
+++ TapVisualEffectViewV2/TapVisualEffectViewV2/TapVisualEffectViewV2.h
@@ -0,0 +1,18 @@
+//
+//  TapVisualEffectViewV2.h
+//  TapVisualEffectViewV2
+//
+//  Created by Osama Rabie on 9/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapVisualEffectViewV2.
+FOUNDATION_EXPORT double TapVisualEffectViewV2VersionNumber;
+
+//! Project version string for TapVisualEffectViewV2.
+FOUNDATION_EXPORT const unsigned char TapVisualEffectViewV2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapVisualEffectViewV2/PublicHeader.h>
+
+
